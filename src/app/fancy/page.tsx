489b4da94af2058173465000d9dee13e32cf96df
Akshay Sunil <sunilakshay653@gmail.com>
2025-06-10 17:32:13 +0530

    Initial Commit

---
+++ src/app/fancy/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const miniweb = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        <h1 className='text-4xl'>Here Lies The MiniWeb</h1>
+    </div>
+  )
+}
+
+export default miniweb
